352c4a94c145d4063ed682f39715ceeafe78a95e
tj_emilz <efourgnier94370@gmail.com>
2025-03-28 20:24:37 +0100

    Test ci/cd 2

--- app/payment/page.js
+++ app/payment/page.js
@@ -7,7 +7,7 @@ const PaymentPage = () => {
 
     return (
         <div className="flex flex-col pt-8">
-            <div className="bg-blue-800 rounded-lg mx-auto px-9 py-3 mb-6 max-w-md">
+            <div className="bg-green-800 rounded-lg mx-auto px-9 py-3 mb-6 max-w-md">
                 <h1 className="text-4xl font-bold text-center text-white">Tickets to Bruxelles</h1>
             </div>
             <iframe id="haWidget" allowtransparency="true" src="https://www.helloasso.com/associations/welcom-evry/evenements/voyage-bruxelles/widget" className="justify-center h-screen"></iframe>
